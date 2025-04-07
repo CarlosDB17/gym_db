0203e4a9388f6d2f25173d7735da62f77ea5d494
Carlos DB <carlosdbtic@gmail.com>
2025-04-07 10:33:15 +0200

    Agregar colores personalizados y actualizar título de la aplicación

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color naranjaBrillante = Color(0xFFF26C21);
+  static const Color verdeVibrante = Color(0xFF7CC145);
+  static const Color naranjaOscuro = Color(0xFFD45500);
+  static const Color verdeOscuro = Color(0xFF4C9B2B);
+  static const Color blanco = Color(0xFFFFFFFF);
+}
